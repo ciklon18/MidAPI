9ad1e315edbe0321ccb0d152f1f22a8016f7cb22
pupov <pupov435@gmail.com>
2023-12-30 18:18:59 +0500

    - added DoctorController.cs - added different models without validation

---
+++ MisAPI/Mappers/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using MisAPI.Entities;
+using MisAPI.Models.Api;
+
+namespace MisAPI.Mappers;
+
+public static class Mapper
+{
+    public static DoctorModel EntityDoctorToDoctorDto(Doctor doctor)
+    {
+        return new DoctorModel
+        {
+            Id = doctor.Id,
+            CreateTime = doctor.CreateTime.ToUniversalTime(),
+            Name = doctor.Name,
+            Birthday = doctor.Birthday.ToUniversalTime(),
+            Gender = doctor.Gender,
+            Email = doctor.Email,
+            Phone = doctor.Phone ?? string.Empty
+        };
+    }
+}
