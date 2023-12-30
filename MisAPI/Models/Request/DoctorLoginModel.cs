f7bbbf852d02700250782b32b5375e9d98fa3205
pupov <pupov435@gmail.com>
2023-12-30 18:22:37 +0500

    - added models with validation

---
+++ MisAPI/Models/Request/DoctorLoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MisAPI.Configurations;
+
+namespace MisAPI.Models.Request;
+
+public class DoctorLoginModel
+{
+    public DoctorLoginModel(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+
+    [StringLength(maximumLength: 100, MinimumLength = 5, ErrorMessage = EntityConstants.ShortOrLongEmailError)]
+    [RegularExpression(pattern: EntityConstants.EmailRegex,
+        ErrorMessage = EntityConstants.IncorrectEmailError)]
+    public string Email { get; set; }
+
+    [StringLength(maximumLength: 100, MinimumLength = 6, ErrorMessage = EntityConstants.ShortPasswordError)]
+    [RegularExpression(pattern: EntityConstants.PasswordRegex,
+        ErrorMessage = EntityConstants.IncorrectPasswordError)]
+    public string Password { get; set; }
+}
